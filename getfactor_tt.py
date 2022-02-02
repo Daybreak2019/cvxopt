ddec1bf5dd07f3c2fcd3ea0f0fbf566afadf61fb
Daybreak2019 <li.wen@wsu.edu>
2022-02-01 22:31:48 -0800

    tests

--- getfactor_tt.py
+++ getfactor_tt.py
@@ -2,12 +2,11 @@ import math
 from cvxopt import base, blas, lapack, cholmod, misc_solvers
 from cvxopt.base import matrix, spmatrix
 
-def GetF ():
-   A = spmatrix([10, 3, 5, -2, 5, 2], [0, 2, 1, 3, 2, 3], [0, 0, 1, 1, 2, 3])
-   Fn = cholmod.symbolic(A)
-   cholmod.numeric(A, Fn)
-   return Fn
+def Compute (F):    
+    cholmod.getfactor(F)
 
-F = GetF ()
+A = spmatrix([10, 3, 5, -2, 5, 2], [0, 2, 1, 3, 2, 3], [0, 0, 1, 1, 2, 3])
+F = cholmod.symbolic(A)
+cholmod.numeric(A, F)
 
-cholmod.getfactor(F)
+Compute (F)
--- spsolve_tt.py
+++ spsolve_tt.py
@@ -2,14 +2,12 @@ import math
 from cvxopt import base, blas, lapack, cholmod, misc_solvers
 from cvxopt.base import matrix, spmatrix
 
-def GetF ():
-   A = spmatrix([10, 3, 5, -2, 5, 2], [0, 2, 1, 3, 2, 3], [0, 0, 1, 1, 2, 3])
-   Fn = cholmod.symbolic(A)
-   cholmod.numeric(A, Fn)
-   return Fn
+def Compute (F):
+    X = spmatrix(1.0,range(4),range(4))
+    cholmod.spsolve(F, X)
+    
+A = spmatrix([10, 3, 5, -2, 5, 2], [0, 2, 1, 3, 2, 3], [0, 0, 1, 1, 2, 3])
+F = cholmod.symbolic(A)
+cholmod.numeric(A, F)
 
-X = spmatrix(1.0,range(4),range(4))
-
-F = GetF ()
-
-cholmod.spsolve(F, X)
+Compute (F)
